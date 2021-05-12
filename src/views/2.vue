b8eb2fa080cdafae532870376289e55ccc849c03
Shikino-code <billso2000@gmail.com>
2021-05-12 18:54:54 +0700

    SHIT!!!

---
+++ src/views/2.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-container>
+    <v-col
+
+      cols="12"
+      sm="4"
+    >
+      <v-card
+        class="mx-auto"
+        max-width="344"
+      >
+        <v-img
+          src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
+          height="400px"
+        ></v-img>
+      </v-card>
+    </v-col>
+
+    <template>
+    <v-row
+       
+        align-self="start"
+      >
+
+      <v-col
+        cols="20"
+        align-self="center"
+      >
+      <v-col
+        cols="20"
+        align-self="center"
+      >
+        <v-card
+          class=""
+          max-width="500"
+          
+          
+        >
+          <v-card-title>
+              
+                Pekora
+
+    </v-card-title>
+    
+
+
+
+
+        </v-card>
+        </v-col>
+        </v-col>
+        </v-row>
+  </template>
+
+  </v-container>
+
+    </template >
+  <script>
+    export default {
+      data: () => ({
+      show: false,
+    }),
+  }
+</script>
+  <script>
+    export default {
+      data: () => ({
+      alignments: [
+        'start',
+        'center',
+        'end',
+      ],
+    }),
+  }
+</script>
